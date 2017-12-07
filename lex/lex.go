c5d123068b57dcc6ca65377c2512f0768bc8a826
Andrew Bates <abates@omeganetserv.com>
2017-12-07 11:59:25 -0500

    Lexer

---
+++ lex/lex.go
@@ -0,0 +1,145 @@
+package lex
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+func (t *Token) TokenLiteral() string {
+	return t.Literal
+}
+
+type StateFn func(*Lexer) StateFn
+
+type Lexer struct {
+	state  StateFn
+	input  string
+	start  int
+	pos    int
+	width  int
+	tokens chan Token
+}
+
+func New(input string, startState StateFn) *Lexer {
+	l := &Lexer{
+		state:  startState,
+		input:  input,
+		tokens: make(chan Token, 2),
+	}
+
+	return l
+}
+
+func (l *Lexer) NextToken() Token {
+	for {
+		select {
+		case t := <-l.tokens:
+			return t
+		default:
+			if l.state == nil {
+				return Token{EOF, ""}
+			}
+
+			l.state = l.state(l)
+		}
+	}
+	// unreachable
+}
+
+func (l *Lexer) Next() (r rune) {
+	if l.pos >= len(l.input) {
+		l.width = 0
+		return -1
+	}
+
+	r, l.width = utf8.DecodeRuneInString(l.input[l.pos:])
+	l.pos += l.width
+	return
+}
+
+func (l *Lexer) IgnoreWhitespace() {
+	for IsSpace(l.Next()) {
+		l.Ignore()
+	}
+	l.Backup()
+}
+
+func (l *Lexer) Ignore() {
+	l.start = l.pos
+}
+
+func (l *Lexer) Backup() {
+	l.pos -= l.width
+}
+
+func (l *Lexer) Peek() rune {
+	r := l.Next()
+	l.Backup()
+	return r
+}
+
+func (l *Lexer) Accept(valid string) bool {
+	if strings.IndexRune(valid, l.Next()) >= 0 {
+		return true
+	}
+	l.Backup()
+	return false
+}
+
+func (l *Lexer) AcceptAlpha() {
+	for IsAlpha(l.Next()) {
+	}
+	l.Backup()
+}
+
+func (l *Lexer) AcceptRun(valid string) {
+	for strings.IndexRune(valid, l.Next()) >= 0 {
+	}
+	l.Backup()
+}
+
+func (l *Lexer) Errorf(format string, args ...interface{}) StateFn {
+	l.tokens <- Token{
+		Type:    ILLEGAL,
+		Literal: fmt.Sprintf(format, args...),
+	}
+	return nil
+}
+
+func (l *Lexer) Emit(tokenType TokenType) {
+	l.tokens <- Token{
+		Type:    tokenType,
+		Literal: l.CurrentInput(),
+	}
+	l.start = l.pos
+}
+
+func (l *Lexer) CurrentInput() string {
+	return l.input[l.start:l.pos]
+}
+
+func IsSpace(r rune) bool {
+	return unicode.IsSpace(r)
+}
+
+func IsNumber(r rune) bool {
+	return '0' <= r && r <= '9'
+}
+
+func IsAlpha(r rune) bool {
+	return 'a' <= r && r <= 'z' || 'A' <= r && r <= 'Z' || r == '_'
+}
---
+++ lex/lex_test.go
@@ -0,0 +1,125 @@
+package lex
+
+import "testing"
+
+func TestRuneTests(t *testing.T) {
+	tests := []struct {
+		test  func(rune) bool
+		input rune
+		want  bool
+	}{
+		{IsSpace, ' ', true},
+		{IsSpace, '\t', true},
+		{IsSpace, '\r', true},
+		{IsSpace, '\n', true},
+		{IsSpace, 'a', false},
+		{IsNumber, '0', true},
+		{IsNumber, '1', true},
+		{IsNumber, '2', true},
+		{IsNumber, '3', true},
+		{IsNumber, '4', true},
+		{IsNumber, '5', true},
+		{IsNumber, '6', true},
+		{IsNumber, '7', true},
+		{IsNumber, '8', true},
+		{IsNumber, '9', true},
+		{IsNumber, 'a', false},
+		{IsAlpha, 'a', true},
+		{IsAlpha, 'z', true},
+		{IsAlpha, 'A', true},
+		{IsAlpha, 'Z', true},
+		{IsAlpha, '_', true},
+		{IsAlpha, '0', false},
+	}
+
+	for i, test := range tests {
+		got := test.test(test.input)
+		if test.want != got {
+			t.Errorf("tests[%d] rune %c wanted %v got %v", i, test.input, test.want, got)
+		}
+	}
+}
+
+func TestLexing(t *testing.T) {
+	word := TokenType("WORD")
+	number := TokenType("NUMBER")
+	punctuation := TokenType("PUNCTUATION")
+
+	var lex StateFn
+	var lexWord StateFn
+	var lexNumber StateFn
+	var lexPunctuation StateFn
+
+	lex = func(lexer *Lexer) StateFn {
+		if lexer.Peek() == -1 {
+			return nil
+		}
+
+		lexer.IgnoreWhitespace()
+		if IsAlpha(lexer.Peek()) {
+			return lexWord
+		}
+
+		if IsNumber(lexer.Peek()) {
+			return lexNumber
+		}
+
+		return lexPunctuation
+	}
+
+	lexWord = func(lexer *Lexer) StateFn {
+		lexer.AcceptAlpha()
+		lexer.Emit(word)
+		return lex
+	}
+
+	lexNumber = func(lexer *Lexer) StateFn {
+		lexer.AcceptRun("0123456789")
+		lexer.Emit(number)
+		return lex
+	}
+
+	lexPunctuation = func(lexer *Lexer) StateFn {
+		if !lexer.Accept(":") {
+			lexer.Next()
+			lexer.Errorf("bad punctuation")
+		} else {
+			lexer.Emit(punctuation)
+		}
+		return lex
+	}
+
+	input := "this is a string of words and numbers: 1234567890abc."
+	expected := []Token{
+		{word, "this"},
+		{word, "is"},
+		{word, "a"},
+		{word, "string"},
+		{word, "of"},
+		{word, "words"},
+		{word, "and"},
+		{word, "numbers"},
+		{punctuation, ":"},
+		{number, "1234567890"},
+		{word, "abc"},
+		{ILLEGAL, "bad punctuation"},
+	}
+
+	lexer := New(input, lex)
+	for _, e := range expected {
+		token := lexer.NextToken()
+		if token != e {
+			t.Errorf("Expected %v got %v", e, token)
+		}
+
+		if token.TokenLiteral() != e.Literal {
+			t.Errorf("Expected %v got %v", e.Literal, token.TokenLiteral())
+		}
+	}
+
+	token := lexer.NextToken()
+	e := Token{EOF, ""}
+	if token != e {
+		t.Errorf("Expected %v got %v", e, token)
+	}
+}
